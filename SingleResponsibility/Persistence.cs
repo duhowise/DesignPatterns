68d186c8b933a1fa9e356bcb05b57d7262cf28d3
duhowise <duhwise@gmail.com>
2019-11-25 12:14:47 +0000

    single responsibility

---
+++ SingleResponsibility/Persistence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace SingleResponsibility
+{
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string fileName, bool overWrite = false)
+        {
+            if (overWrite || File.Exists(fileName))
+            {
+                File.WriteAllText(fileName, journal.ToString());
+            }
+        }
+
+        public void Save(string filename)
+        {
+            File.WriteAllText(filename, ToString());
+        }
+
+        public static Journal Load(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Journal Load(Uri uri)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SingleResponsibility/Program.cs
+++ SingleResponsibility/Program.cs
@@ -1,46 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Diagnostics;
 
 namespace SingleResponsibility
 {
-    public class Journal
-    {
-        private readonly List<string> entries = new List<string>();
-        private static int count = 0;
-
-        public int AddEntry(string text)
-        {
-            entries.Add($"{++count}: {text}");
-            return count;
-        }
-
-        public void RemoveEntry(int index)
-        {
-            entries.RemoveAt(index);
-        }
-
-        public override string ToString()
-        {
-            return string.Join(Environment.NewLine, entries);
-        }
-
-        public void Save(string filename)
-        {
-            File.WriteAllText(filename, ToString());
-        }
-
-        public static Journal Load(string fileName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public static Journal Load(Uri uri)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     internal class Program
     {
         private static void Main(string[] args)
@@ -49,6 +11,10 @@ namespace SingleResponsibility
             journal.AddEntry("I cried today");
             journal.AddEntry("I ate a bug");
             Console.WriteLine(journal);
+            var persist = new Persistence();
+            var fileName = "journal.txt";
+            persist.SaveToFile(journal, fileName, true);
+            Process.Start(fileName);
             Console.ReadLine();
         }
     }
